f9f619fc0794dfac67cdb7b68bbc36af6ec37fb7
aayushadhikari93 <aayushhhhhh93@gmail.com>
2024-12-09 21:48:49 +0545

    Complete Swap Variable

--- basic/Program.cs
+++ basic/Program.cs
@@ -10,6 +10,17 @@ namespace basic
     {
         static void Main(string[] args)
         {
+            int a, b;
+            Console.WriteLine("Enter two numbers to swap");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Number before swapping:a={a},b={b}");
+            a = a + b;
+            b = a - b;
+            a= a - b;
+            Console.WriteLine($"Number after swapping:a={a},b={b}");
+            Console.ReadLine();
+            
         }
     }
 }
